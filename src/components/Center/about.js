419adf8ca46cc0655ae239db467338176337e6e8
Andrea Massignan <73525197+duchannes19@users.noreply.github.com>
2023-07-02 20:57:52 +0200

    Minor Update

--- src/components/Center/about.js
+++ src/components/Center/about.js
@@ -6,8 +6,8 @@ const About = () => {
         <h4>About Me</h4>
         <>
         <p>Hi I am Andrea, full stack developer.</p>
-        <p style={{margin: '1rem'}}>I am a Computer Engineering graduate and have a passion for creating innovative technology solutions.</p>
-        <p>I would like to put my knowledge and skills to use to contribute to the development of quality software.</p>
+        <p style={{margin: '1rem'}}>I am a Computer Engineering graduate and have a passion for creating and experimenting with programming languages.</p>
+        <p>I want to put my knowledge and skills to contribute to the development of quality software.</p>
         <p>I am motivated to continuously learn and take on complex challenges.</p>
         <img src={pc} alt='about' className='about-gif'/>
         </>
